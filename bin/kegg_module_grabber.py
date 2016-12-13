a311554eb53ec76e445c4ad28053b671dfe18786
geronimp <joel.boyd@uqconnect.edu.au>
2016-12-13 15:25:12 +1000

    can now include manually defined modules in KEGG format

--- bin/kegg_module_grabber.py
+++ bin/kegg_module_grabber.py
@@ -62,10 +62,21 @@ class KeggModuleGrabber:
         self.m = pickle.load(open('.'.join([self.M, 
                                             self.VERSION, self.PICKLE])))
         logging.info("Done!")
+        
+    def _update_with_custom_modules(self, custom_modules):
+        custom_modules_dict = {line.split('\t')[0]:line.strip().split('\t')[1]
+                               for line in open(custom_modules)}
+        self.m2def.update(custom_modules_dict)
+        
+        for key in custom_modules_dict.keys():
+            self.m[key]='Custom'
             
     def main(self, args):
         
         if args.subparser_name == self.ANNOTATE:
+            if args.custom_modules:
+                self._update_with_custom_modules(args.custom_modules)
+
             output_path = args.output_prefix + '_annotations.tsv'
     
             genome_to_ko_sets = {}
