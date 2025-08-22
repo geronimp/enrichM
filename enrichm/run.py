5e68ac9ab836e5a671fd696d5ff71ca91cece780
Joel <joel.anthonyb@gmail.com>
2025-08-22 16:06:21 +1000

    (BIO-8): linting, removing unused options in enrich'

--- enrichm/run.py
+++ enrichm/run.py
@@ -331,20 +331,31 @@ class Run:
     def run_enrichment(self, args):
         self._check_enrichment(args)
         enrichment = Enrichment()
-        enrichment.enrichment_pipeline(# Input options
-                                        args.annotate_output, args.annotation_matrix, args.gff_files,
-                                        args.metadata, args.abundance, args.abundance_metadata,
-                                        args.transcriptome, args.transcriptome_metadata,
-                                        # Runtime options
-                                        args.pval_cutoff, args.proportions_cutoff, 
-                                        args.threshold, args.multi_test_correction, 
-                                        args.batchfile, args.processes, 
-                                        args.allow_negative_values, args.ko, args.pfam, 
-                                        args.tigrfam, args.cluster, args.ortholog, args.cazy,
-                                        args.ec, args.ko_hmm, args.range, args.subblock_size,
-                                        args.operon_mismatch_cutoff, args.operon_match_score_cutoff,
-                                        # Outputs
-                                        args.output)
+        enrichment.enrichment_pipeline(args.annotate_output,
+                                       args.annotation_matrix,
+                                       args.gff_files,
+                                       args.metadata,
+                                       args.abundance,
+                                       args.abundance_metadata,
+                                       args.pval_cutoff,
+                                       args.proportions_cutoff,
+                                       args.threshold,
+                                       args.multi_test_correction,
+                                       args.batchfile,
+                                       args.processes,
+                                       args.ko,
+                                       args.pfam,
+                                       args.tigrfam,
+                                       args.cluster,
+                                       args.ortholog,
+                                       args.cazy,
+                                       args.ec,
+                                       args.ko_hmm,
+                                       args.range,
+                                       args.subblock_size,
+                                       args.operon_mismatch_cutoff,
+                                       args.operon_match_score_cutoff,
+                                       args.output)
 
     def run_network(self, args):
         self._check_network(args)
